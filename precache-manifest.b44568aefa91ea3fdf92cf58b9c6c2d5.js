b8d15526040afd6ef9cfb457a59a3e8bff9bbbd7
Kurokikai <kurokikai@Kurokikais-MacBook-Pro.local>
2019-09-23 01:46:33 +1200

    Initial dist subtree commit

---
+++ precache-manifest.b44568aefa91ea3fdf92cf58b9c6c2d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/travel-site/robots.txt"
+  },
+  {
+    "revision": "7375a36d3a45d8ac8c7e",
+    "url": "/travel-site/js/chunk-vendors.d4fb9efb.js"
+  },
+  {
+    "revision": "818732f2357f106c9185",
+    "url": "/travel-site/js/app.db636017.js"
+  },
+  {
+    "revision": "9cb7dc98253c7dd9c11b",
+    "url": "/travel-site/js/about.24fb71c2.js"
+  },
+  {
+    "revision": "1ffc11de00eac39b71e2cfe2db30ab3e",
+    "url": "/travel-site/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/travel-site/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "818732f2357f106c9185",
+    "url": "/travel-site/css/app.80fbf154.css"
+  }
+];
